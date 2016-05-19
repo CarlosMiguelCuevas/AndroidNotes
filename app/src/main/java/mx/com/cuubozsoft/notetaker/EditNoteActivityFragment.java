352203a2381bc76029ecec0a4443426e68e0c689
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-05-18 19:19:24 -0700

    final step

--- app/src/main/java/mx/com/cuubozsoft/notetaker/EditNoteActivityFragment.java
+++ app/src/main/java/mx/com/cuubozsoft/notetaker/EditNoteActivityFragment.java
@@ -1,51 +1,79 @@
 package mx.com.cuubozsoft.notetaker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Optional;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class EditNoteActivityFragment extends Fragment {
-
+public class EditNoteActivityFragment extends Fragment
+{
+    public static final int RESULT_DELETE = -500;
     private boolean isInEditMode = true;
+    private boolean isAddingNote = true;
 
     public EditNoteActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         View rootView = inflater.inflate(R.layout.fragment_edit_note, container, false);
 
+        setHasOptionsMenu(true);
+
         Button saveButton = (Button) rootView.findViewById(R.id.saveButton);
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        EditText titleEditText = (EditText)rootView.findViewById(R.id.editTextTitle);
+        EditText noteEditText = (EditText) rootView.findViewById(R.id.multilineTextNote);
+        TextView viewDate = (TextView) rootView.findViewById(R.id.textViewDate);
+
+        Intent editInetnt = getActivity().getIntent();
+        Optional<Serializable> noteData = Optional.ofNullable(editInetnt.getSerializableExtra("Note"));
+
+        noteData.ifPresent(info -> {
+            Note editableNote = (Note)info;
+            titleEditText.setText(editableNote.getTitle());
+            noteEditText.setText(editableNote.getContent());
+            DateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            viewDate.setText(dateformat.format(editableNote.getDate()));
+
+            isInEditMode = false;
+            titleEditText.setEnabled(isInEditMode);
+            noteEditText.setEnabled(isInEditMode);
+
+            saveButton.setText(R.string.edit);
+            isAddingNote = false;
+        });
+
+        cancelButton.setOnClickListener(view -> {
+            getActivity().setResult(Activity.RESULT_CANCELED,new Intent());
+            getActivity().finish();
+        });
 
         saveButton.setOnClickListener((View v) -> {
-            EditText titleEditText = (EditText)rootView.findViewById(R.id.editTextTitle);
-            EditText noteEditText = (EditText) rootView.findViewById(R.id.multilineTextNote);
-            TextView viewDate = (TextView) rootView.findViewById(R.id.textViewDate);
-            String buttonCaptionNestState = "";
-            String date = "";
-
             if(isInEditMode)
             {
-//                buttonCaptionNestState = "Edit";
-                DateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-                date = dateformat.format(Calendar.getInstance().getTime());
-                viewDate.setText(date);
-
                 Note note = new Note(titleEditText.getText().toString(),noteEditText.getText().toString(),Calendar.getInstance().getTime());
 
                 Intent returnIntent = new Intent();
@@ -58,16 +86,52 @@ public class EditNoteActivityFragment extends Fragment {
             }
             else
             {
-//                buttonCaptionNestState = "Save";
+                isInEditMode = true;
+                saveButton.setText(R.string.save);
+                titleEditText.setEnabled(isInEditMode);
+                noteEditText.setEnabled(isInEditMode);
             }
-
-//            saveButton.setText(buttonCaptionNestState);
-//            isInEditMode = !isInEditMode;
-//            titleEditText.setEnabled(isInEditMode);
-//            noteEditText.setEnabled(isInEditMode);
-
         });
 
         return rootView;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        if(! isAddingNote){
+            inflater.inflate(R.menu.menu_main,menu);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.confirm_delete);
+            builder.setMessage(R.string.alert_delete);
+            builder.setPositiveButton(R.string.delete, (dialog, i)->{
+
+                Intent returnIntent = new Intent();
+
+                getActivity().setResult(RESULT_DELETE,returnIntent);
+                getActivity().finish();
+
+            });
+            builder.setNegativeButton(R.string.cancel_button,(dialog, i)->{
+
+            });
+            builder.create().show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/mx/com/cuubozsoft/notetaker/ListNotes.java
+++ app/src/main/java/mx/com/cuubozsoft/notetaker/ListNotes.java
@@ -3,26 +3,48 @@ package mx.com.cuubozsoft.notetaker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ListNotes extends AppCompatActivity
 {
     private List<Note> noteList = new ArrayList<>();
     private int REQUEST_CODE = 1;
+    private ListView notesListView;
+    private int actualNote = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_notes);
+
+
+        notesListView = (ListView) findViewById(R.id.noteListView);
+        notesListView.setOnItemClickListener(
+                (adapterView, view, itemIndex, l) -> {
+                    Intent editIntent = new Intent(view.getContext(),EditNoteActivity.class);
+                    editIntent.putExtra("Note",noteList.get(itemIndex));
+                    actualNote = itemIndex;
+                    startActivityForResult(editIntent,REQUEST_CODE);
+                }
+        );
+
+        registerForContextMenu(notesListView);
+
         noteList.add(new Note("first note","bla bla",new Date()));
         noteList.add(new Note("second note","bla bla",new Date()));
         noteList.add(new Note("third note","bla bla",new Date()));
@@ -55,9 +77,63 @@ public class ListNotes extends AppCompatActivity
         return true;
     }
 
-    private void populateList() {
-        ListView notesListView = (ListView) findViewById(R.id.noteListView);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == RESULT_CANCELED)
+        {
+            return;
+        }
 
+        if(resultCode == EditNoteActivityFragment.RESULT_DELETE)
+        {
+            noteList.remove(actualNote);
+            actualNote = -1;
+            populateList();
+
+        }
+
+        Optional<Serializable> result = Optional.ofNullable(data.getSerializableExtra("Note"));
+
+        result.ifPresent(value -> {
+            Note newNote =(Note)value;
+
+            if(actualNote == -1)
+            {
+                noteList.add(newNote);
+            }
+            else
+            {
+                noteList.set(actualNote,newNote);
+                actualNote = -1;
+            }
+            populateList();
+        });
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        noteList.remove(info.position);
+
+        populateList();
+
+        return true;
+    }
+
+    private void populateList()
+    {
         List<String> notelist = noteList.stream().map(n -> n.getTitle()).collect(Collectors.toList());
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, notelist);
